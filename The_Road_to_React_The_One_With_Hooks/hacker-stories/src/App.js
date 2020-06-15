6265baae8034048d3b374327963d840237e9e25f
oggyman <oggyla@gmail.com>
2020-06-15 13:19:51 -0700

    React JSX

--- The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
+++ The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
-const title = "React";
+function getTitle(title) {
+    return title;
+}
 
 function App() {
     return (
         <>
-            <h1>Hello World</h1>
-            
+            <h1>Hello {getTitle('React')}</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" />
         </>
     );
 }
