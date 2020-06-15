88c6a8a8fe508bf49b82033ccbe1b77f2c1d9cae
oggyman <oggyla@gmail.com>
2020-06-15 13:13:34 -0700

    initial commit

---
+++ The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const title = "React";
+
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            
+        </>
+    );
+}
+
+export default App;
