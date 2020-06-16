b72db0e4369db3ea6c0ebef839b6b92704be6da7
oggyman <oggyla@gmail.com>
2020-06-16 12:12:15 -0700

    Lifting State in React

--- The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
+++ The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
@@ -18,11 +18,41 @@ const App = () => {
             points: 5,
             objectID: 1,
         },
+        {
+            title: 'Redux qwe',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 2,
+        },
+        {
+            title: 'Redux asd',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 3,
+        },
+        {
+            title: 'Redux zxc',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 4,
+        },
     ];
 
+    const [searchTerm, setSearchTerm] = React.useState('');
+
     const handleSearch = event => {
-        console.log(event.target.value);
+        setSearchTerm(event.target.value);
     };
+
+    const searchedStories = stories.filter(story =>
+        story.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <div>
@@ -32,30 +62,17 @@ const App = () => {
 
             <hr />
 
-            <List list={stories} />
+            <List list={searchedStories} />
         </div>
     );
 };
 
-const Search = props => {
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = event => {
-        setSearchTerm(event.target.value);
-
-        props.onSearch(event);
-    };
-
-    return (
-        <div>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={handleChange} />
-            <p>
-                Searching for <strong>{searchTerm}</strong>.
-      </p>
-        </div>
-    );
-};
+const Search = props => (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch} />
+    </div>
+);
 
 const List = props =>
     props.list.map(item => (
