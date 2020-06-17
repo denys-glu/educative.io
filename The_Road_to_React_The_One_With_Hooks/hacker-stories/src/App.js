ecd527c05b759177294867d465adee36bfe0ca7f
oggyman <oggyla@gmail.com>
2020-06-17 14:12:35 -0700

    React Impossible States using complex Reducer logic

--- The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
+++ The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
@@ -1,182 +1,201 @@
 import React from 'react';
 
 const initialStories = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
 ];
 
 const getAsyncStories = () =>
-    new Promise(resolve =>
-        setTimeout(
-            () => resolve({ data: { stories: initialStories } }),
-            2000
-        )
-    );
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve({ data: { stories: initialStories } }),
+      2000
+    )
+  );
 
 const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = React.useState(
-        localStorage.getItem(key) || initialState
-    );
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
-    React.useEffect(() => {
-        localStorage.setItem(key, value);
-    }, [value, key]);
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
 
-    return [value, setValue];
+  return [value, setValue];
 };
 
 const storiesReducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_STORIES':
-            return action.payload;
-        case 'REMOVE_STORY':
-            return state.filter(
-                story => action.payload.objectID !== story.objectID
-            );
-        default:
-            throw new Error();
-    }
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          story => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
 };
 
 const App = () => {
-    const [searchTerm, setSearchTerm] = useSemiPersistentState(
-        'search',
-        'React'
-    );
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
 
-    const [stories, dispatchStories] = React.useReducer(
-        storiesReducer,
-        []
-    );
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [isError, setIsError] = React.useState(false);
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], isLoading: false, isError: false }
+  );
 
-    React.useEffect(() => {
-        setIsLoading(true);
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-        getAsyncStories()
-            .then(result => {
-                dispatchStories({
-                    type: 'SET_STORIES',
-                    payload: result.data.stories,
-                });
-                setIsLoading(false);
-            })
-            .catch(() => setIsError(true));
-    }, []);
-
-    const handleRemoveStory = item => {
+    getAsyncStories()
+      .then(result => {
         dispatchStories({
-            type: 'REMOVE_STORY',
-            payload: item,
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.data.stories,
         });
-    };
+      })
+      .catch(() =>
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+      );
+  }, []);
 
-    const handleSearch = event => {
-        setSearchTerm(event.target.value);
-    };
+  const handleRemoveStory = item => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
 
-    const searchedStories = stories.filter(story =>
-        story.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
 
-    return (
-        <div>
-            <h1>My Hacker Stories</h1>
+  const searchedStories = stories.data.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-            <InputWithLabel
-                id="search"
-                value={searchTerm}
-                isFocused
-                onInputChange={handleSearch}
-            >
-                <strong>Search:</strong>
-            </InputWithLabel>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-            <hr />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
-            {isError && <p>Something went wrong ...</p>}
+      <hr />
 
-            {isLoading ? (
-                <p>Loading ...</p>
-            ) : (
-                    <List
-                        list={searchedStories}
-                        onRemoveItem={handleRemoveStory}
-                    />
-                )}
-        </div>
-    );
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
+    </div>
+  );
 };
 
 const InputWithLabel = ({
-    id,
-    value,
-    type = 'text',
-    onInputChange,
-    isFocused,
-    children,
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
 }) => {
-    const inputRef = React.useRef();
+  const inputRef = React.useRef();
 
-    React.useEffect(() => {
-        if (isFocused) {
-            inputRef.current.focus();
-        }
-    }, [isFocused]);
+  React.useEffect(() => {
+    if (isFocused) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
 
-    return (
-        <>
-            <label htmlFor={id}>{children}</label>
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
-            <input
-                ref={inputRef}
-                id={id}
-                type={type}
-                value={value}
-                onChange={onInputChange}
-            />
-        </>
-    );
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
 };
 
 const List = ({ list, onRemoveItem }) =>
-    list.map(item => (
-        <Item
-            key={item.objectID}
-            item={item}
-            onRemoveItem={onRemoveItem}
-        />
-    ));
+  list.map(item => (
+    <Item
+      key={item.objectID}
+      item={item}
+      onRemoveItem={onRemoveItem}
+    />
+  ));
 
 const Item = ({ item, onRemoveItem }) => (
-    <div>
-        <span>
-            <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-            <button type="button" onClick={() => onRemoveItem(item)}>
-                Dismiss
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
       </button>
-        </span>
-    </div>
+    </span>
+  </div>
 );
 
 export default App;
