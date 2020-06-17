72267853aeebe8c7ff475b16733482a7884a9096
oggyman <oggyla@gmail.com>
2020-06-17 10:22:23 -0700

    React Custom Hooks (Advanced)

--- The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
+++ The_Road_to_React_The_One_With_Hooks/hacker-stories/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const useSemiPersistentState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    );
+
+    React.useEffect(() => {
+        localStorage.setItem(key, value);
+    }, [value, key]);
+
+    return [value, setValue];
+};
+
 const App = () => {
     const stories = [
         {
@@ -20,7 +32,10 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState('React');
+    const [searchTerm, setSearchTerm] = useSemiPersistentState(
+        'search',
+        'React'
+    );
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
@@ -43,28 +58,30 @@ const App = () => {
     );
 };
 
-const Search = props => (
+const Search = ({ search, onSearch }) => (
     <div>
         <label htmlFor="search">Search: </label>
         <input
             id="search"
             type="text"
-            value={props.search}
-            onChange={props.onSearch}
+            value={search}
+            onChange={onSearch}
         />
     </div>
 );
 
-const List = props =>
-    props.list.map(item => (
-        <div key={item.objectID}>
-            <span>
-                <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-        </div>
-    ));
+const List = ({ list }) =>
+    list.map(item => <Item key={item.objectID} item={item} />);
+
+const Item = ({ item }) => (
+    <div>
+        <span>
+            <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+    </div>
+);
 
 export default App;
